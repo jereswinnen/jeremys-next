b43212b54502dcd461a18c8b149cd53cccdbd9d1
Jeremy Swinnen <hey@jeremys.be>
2025-02-14 20:44:15 +0100

    Setting up Tailwind theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} text-stone-900 dark:text-white bg-white dark:bg-stone-950 antialiased`}
       >
         {children}
       </body>
