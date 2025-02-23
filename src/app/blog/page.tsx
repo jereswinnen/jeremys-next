a4c9946a5d9bec5b0092c0fb332f1a21035899c0
Jeremy Swinnen <hey@jeremys.be>
2025-02-23 17:00:24 +0100

    Sorting blogroll alphabetically and updating rel tags

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -9,9 +9,18 @@ export const metadata: Metadata = {
   title: "Blog - Jeremy Swinnen",
 };
 
+function getLastName(fullName: string): string {
+  const parts = fullName.split(" ");
+  return parts[parts.length - 1].toLowerCase();
+}
+
 export default async function Blog() {
   const posts = await getAllPosts();
   const topics = await getTopicsWithCounts();
+
+  const sortedBlogroll = [...blogroll].sort((a, b) =>
+    getLastName(a.name).localeCompare(getLastName(b.name))
+  );
 
   return (
     <>
@@ -37,13 +46,31 @@ export default async function Blog() {
             </h3>
             <ul className="pl-3 flex flex-col gap-2 border-l border-stone-950/15 dark:border-white/15 [&>li]:text-sm [&>li]:ease-in-out [&>li]:transition-all [&>li]:duration-300 [&>li]:hover:opacity-60 [&>li]:hover:translate-x-1">
               <li>
-                <a href="https://jeremys.be/feed.xml">RSS</a>
+                <a
+                  href="https://jeremys.be/feed.xml"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  RSS
+                </a>
               </li>
               <li>
-                <a href="https://threads.net/@jereswinnen">Threads</a>
+                <a
+                  href="https://threads.net/@jereswinnen"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  Threads
+                </a>
               </li>
               <li>
-                <a href="https://mstdn.social/@jeremys">Mastodon</a>
+                <a
+                  href="https://mstdn.social/@jeremys"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  Mastodon
+                </a>
               </li>
             </ul>
           </div>
@@ -52,16 +79,12 @@ export default async function Blog() {
               Blogroll
             </h3>
             <ul className="pl-3 flex flex-col gap-2 border-l border-stone-950/15 dark:border-white/15">
-              {blogroll.map((blog) => (
+              {sortedBlogroll.map((blog) => (
                 <li
                   key={blog.url}
                   className="text-sm ease-in-out duration-300 transition-all hover:opacity-60 hover:translate-x-1"
                 >
-                  <Link
-                    href={blog.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
+                  <Link href={blog.url} target="_blank" rel="noopener">
                     {blog.name}
                   </Link>
                 </li>
